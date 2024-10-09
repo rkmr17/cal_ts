e66a76a0026bc9554638d7cf5ebc2db2a633642c
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2024-10-09 21:06:13 +0900

    division関数

--- src/main.c
+++ src/main.c
@@ -12,7 +12,13 @@ int multiplica(int num1, int num2) {
   return num1 * num2;
 }
 
-
+int division(int num1, int num2) {
+  if(num2 == 0) {
+    printf("0では割り切れません");
+    return 0;
+  }
+  return num1 / num2;
+}
 
 int main() {
   printf("hello\n");
